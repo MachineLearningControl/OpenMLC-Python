a98d53d1ea74f15aaa292e61f33395173c0dad7f
Raul Lopez <raulopez0@gmail.com>
2016-06-11 20:01:04 -0300

    #1 use Individual::generate instead o matlab generate function in BaseCreation class

--- MLC/Population/Creation/BaseCreation.py
+++ MLC/Population/Creation/BaseCreation.py
@@ -1,6 +1,7 @@
 import MLC.Log.log as lg
 import numpy as np
 
+from MLC.individual.Individual import Individual
 
 class BaseCreation(object):
     def __init__(self, eng, config):
@@ -18,13 +19,14 @@ class BaseCreation(object):
 
     def _fill_creation(self, individuals, index, type):
         while index < len(individuals):
-            indiv = self._eng.MLCind()
+
+            indiv = Individual()
             param = self._eng.eval('wmlc.parameters')
-            self._eng.generate(indiv, param, type)
+            indiv.generate(param, type)
 
             table = self._eng.eval('wtable')
             # Returns (individual, number, repeated)
-            response = self._eng.add_individual(table, indiv, nargout=3)
+            response = self._eng.add_individual(table, indiv.get_matlab_object(), nargout=3)
 
             if not response[2]:
                 # The individual didn't exist
@@ -33,12 +35,12 @@ class BaseCreation(object):
                 lg.logger_.info('[FILL_CREATION] Generating individual N#' +
                                 str(indiv_number))
 
-                self._eng.workspace['windiv'] = indiv
+                self._eng.workspace['windiv'] = indiv.get_matlab_object()
                 lg.logger_.debug('[FILL_CREATION] Individual N#' +
                                  str(indiv_number) +
                                  ' - Value: ' + self._eng.eval('windiv.value'))
 
-                if self._eng.preev(indiv, param, nargout=1):
+                if self._eng.preev(indiv.get_matlab_object(), param, nargout=1):
                     self._individuals.append((index, response[1]))
                     index += 1
                 else:
