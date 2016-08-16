94b956f1b8fb21647123dc113f5045dd11f8d007
Ezequiel Torres <ezequiel.torresfeyuk@gmail.com>
2016-08-16 18:50:05 -0300

    Added MLC_dir as an argument in Python Function Finder script
    * #12

--- scripts/MATLAB_functions_finder.py
+++ scripts/MATLAB_functions_finder.py
@@ -1,14 +1,21 @@
 import os
 import re
+import argparse
 from tabulate import tabulate
 
 
-def retrieve_python_files():
+def usage():
+    print """
+./MATLAB_functions_finder.py [-d|--MLC_dir]
+    -d|--MLC_dir: Absolute or Relative Path to the directory containing the Python implementation of MLC
+""" 
+
+def retrieve_python_files(mlc_dir):
     list = []
     files_regex = re.compile("^.*py$")
     no_init = re.compile("^(.(?!(__init__)))*$")
 
-    for root, directories, filenames in os.walk('./MLC'):
+    for root, directories, filenames in os.walk(mlc_dir):
         for filename in filenames:
             file_path = os.path.join(root, filename)
             if files_regex.match(file_path) and no_init.match(file_path):
@@ -17,11 +24,11 @@ def retrieve_python_files():
     return list
 
 
-def retrieve_methods(file_list):
+def retrieve_methods():
     engine_regex = re.compile("^.*(self\._eng\..*)$")
     methods = []
 
-    for python_file in retrieve_python_files():
+    for python_file in retrieve_python_files(mlc_dir):
         f = open(python_file, "r")
         i = 0
         for line in f:
@@ -33,7 +40,7 @@ def retrieve_methods(file_list):
     return methods
 
 
-def draw_methods(methods, blacklist = None):
+def draw_methods(methods, blacklist=None):
     # First, show all the methods
     columns = ["File", "Line", "Method"]
     rows = []
@@ -50,16 +57,29 @@ def draw_methods(methods, blacklist = None):
 
     print tabulate(rows, columns, tablefmt="fancy_grid")
 
+# Parse program arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--MLC_dir', required=True)
+args = parser.parse_args()
+args = vars(args)
+mlc_dir = args['MLC_dir']
 
-methods = retrieve_methods(retrieve_python_files())
+# If the path is not valid, 
+if not os.path.isdir(mlc_dir):
+    print "MLC_Dir must be a valid path. Aborting program"
+    usage()
+    exit(-1)
+
+methods = retrieve_methods()
 print "Draw all methods"
 draw_methods(methods)
+print "\n\n\n"
 
 print "Draw all methods filtering getters and setters"
 blacklist = ["get_", "set_"]
 draw_methods(methods, blacklist)
+print "\n\n\n"
 
 blacklist.append("eval")
 print "Draw all methods filtering getters and setters and eval calls"
 draw_methods(methods, blacklist)
-
