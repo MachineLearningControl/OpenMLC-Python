f51f567ccc09824f74dfa46e559d33399a5efe49
Raul Lopez <raulopez0@gmail.com>
2016-10-15 16:53:57 -0300

    #19 fix mlc python path

--- tests/integration_tests/run_tests.sh
+++ tests/integration_tests/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Put the absolute path where the 'shared' python was installed
-export PYTHONPATH=$PYTHONPATH:$HOME/workspace_/mlc_v3
-NOSETESTS=/opt/mlc-python-2.7.11/bin/python
+export PYTHONPATH=$PYTHONPATH:../../
+NOSETESTS=/opt/mlc-python-2.7.11/bin/mlc_python
 
 # Run Integration tests
 $NOSETESTS integration_tests.py $@
