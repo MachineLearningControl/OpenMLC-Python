ba4eafba132dcb4a8fde4d4e481f1d7309c06c87
Marco-GG <marco.gerzb@gmail.com>
2016-10-27 19:55:47 -0300

    Issue #22 : Definiciones de pines en boards arduino
    Subo definiciones de pines digitales y analógicos para boards de arduino

---
+++ MLC/arduino/boards.py
@@ -0,0 +1,5 @@
+
+Due = {"ANALOG_PINS":range(54, 67), "DIGITAL_PINS":range(0,54)}
+Uno = {"ANALOG_PINS":range(14, 20), "DIGITAL_PINS":range(0,14)}
+Mega = {"ANALOG_PINS":range(54, 70), "DIGITAL_PINS":range(0,54)}
+Leonardo = {"ANALOG_PINS":range(14, 20), "DIGITAL_PINS":range(0,14)}
